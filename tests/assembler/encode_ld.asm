66e41eab5d98faa4083bb97e4488f0d00a344521
Tom Lynch <twhlynch@gmail.com>
2026-08-31 13:14:54 +1000

    feat: unfinished encoding tests

---
+++ tests/assembler/encode_ld.asm
@@ -0,0 +1,54 @@
+; ld instruction encoding must match
+; 0010 DR1 PCoffset9
+
+.ORIG x3000
+
+    ld r0, Instruction1
+    ld r1, Encoding1
+
+    add r2, r1, #0
+    not r2, r2
+    add r2, r2, #1
+
+    add r3, r0, r2 ; instruction - encoding
+    brnp Fail
+
+    ; first passed
+
+    ld r0, Instruction2
+    ld r1, Encoding2
+
+    add r2, r1, #0
+    not r2, r2
+    add r2, r2, #1
+
+    add r3, r0, r2 ; instruction - encoding
+    brnp Fail
+
+    ; both passed
+
+    br Pass
+
+           ; 0010 000 100000000
+Instruction1 ld   r0, Instruction1
+Encoding1 .FILL x2100
+
+           ; 0010 110 100000011
+Instruction2 ld   r6, Instruction1
+Encoding2 .FILL x2D03
+
+Fail
+    lea r0, FailMsg
+    puts
+    halt
+
+Pass
+    lea r0, PassMsg
+    puts
+    halt
+
+PassMsg .STRINGZ "TEST_PASSED"
+FailMsg .STRINGZ "TEST_FAILED"
+
+.END
+
---
+++ tests/assembler/encode_ldr.asm
@@ -0,0 +1,54 @@
+; ldr instruction encoding must match
+; 0110 DR1 BR1 offst6
+
+.ORIG x3000
+
+    ld r0, Instruction1
+    ld r1, Encoding1
+
+    add r2, r1, #0
+    not r2, r2
+    add r2, r2, #1
+
+    add r3, r0, r2 ; instruction - encoding
+    brnp Fail
+
+    ; first passed
+
+    ld r0, Instruction2
+    ld r1, Encoding2
+
+    add r2, r1, #0
+    not r2, r2
+    add r2, r2, #1
+
+    add r3, r0, r2 ; instruction - encoding
+    brnp Fail
+
+    ; both passed
+
+    br Pass
+;TODO:
+           ; 0000000000000000
+Instruction1 add r0, r0, r0
+Encoding1 .FILL x0000
+
+           ; 0000000000000000
+Instruction2 add r1, r2, r3
+Encoding2 .FILL x0000
+
+Fail
+    lea r0, FailMsg
+    puts
+    halt
+
+Pass
+    lea r0, PassMsg
+    puts
+    halt
+
+PassMsg .STRINGZ "TEST_PASSED"
+FailMsg .STRINGZ "TEST_FAILED"
+
+.END
+
