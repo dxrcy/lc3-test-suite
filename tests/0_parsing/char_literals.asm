32096f5d1629834d60c60dc5eab5195f7bbd6bc7
Tom Lynch <twhlynch@gmail.com>
2026-09-01 14:09:37 +1000

    feat: character literal test

---
+++ tests/0_parsing/char_literals.asm
@@ -0,0 +1,16 @@
+.ORIG x3000
+
+    halt
+
+    .FILL 'a'
+    .FILL 'z'
+    .FILL '0'
+    .FILL ' '
+    .FILL '\n'
+    .FILL '\t'
+    .FILL '\r'
+    .FILL '\\'
+    .FILL '\''
+    .FILL '\"'
+
+.END
