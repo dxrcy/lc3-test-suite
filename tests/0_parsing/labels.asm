0f59f943e7ce709da5282a5efb20cc5a2e0f554b
Tom Lynch <twhlynch@gmail.com>
2026-09-01 14:00:04 +1000

    feat: labels parsing test

---
+++ tests/0_parsing/labels.asm
@@ -0,0 +1,18 @@
+.ORIG x3000
+
+Label           halt
+_A              halt
+_B_             halt
+__X             halt
+Label123        halt
+CamelCase       halt
+ALLCAPS         halt
+SCREAMING_SNAKE halt
+lowercase       halt
+snake_case      halt
+Under_Score     halt
+a1              halt
+z9              halt
+_test           halt
+
+.END
