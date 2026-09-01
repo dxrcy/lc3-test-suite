08f595a23a51f77d03204ce55b40f25d1caf763d
Tom Lynch <twhlynch@gmail.com>
2026-09-01 13:25:37 +1000

    feat: comment parsing test

---
+++ tests/0_parsing/comments.asm
@@ -0,0 +1,7 @@
+; comment at start
+.ORIG x3000 ; comment after .ORIG
+    halt ; comment after instruction
+    halt;comment not separated by a space
+;   halt instruction and .ORIG directive in a comment
+; full comment line
+.END ; comment at end
