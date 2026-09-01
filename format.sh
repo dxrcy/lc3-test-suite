61335ece74276e090a924cbe6f06742bce33498b
Tom Lynch <twhlynch@gmail.com>
2026-09-01 14:16:20 +1000

    chore: exclude tabs test from formatter

--- format.sh
+++ format.sh
@@ -2,7 +2,19 @@
 
 set -e
 
+ignore=(
+    "whitespace_tabs.asm"
+)
+
 find tests -name '*.asm' -print0 | while IFS= read -r -d '' file; do
+    filename="${file##*/}"
+
+    case "$filename" in
+    "${ignore[@]}")
+        continue
+        ;;
+    esac
+
     perl -0777 -pi -e '
         s/\r//g;
         s/\t/    /g;
