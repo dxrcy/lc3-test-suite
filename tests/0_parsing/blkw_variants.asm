93fa94ae2ac3cbd33866738739e89a4d76c9054d
Tom Lynch <twhlynch@gmail.com>
2026-09-01 14:09:24 +1000

    feat: fill and blkw variants

---
+++ tests/0_parsing/blkw_variants.asm
@@ -0,0 +1,10 @@
+.ORIG x3000
+
+    halt
+
+    .BLKW #0
+    .BLKW #1
+    .BLKW x1
+    .BLKW 1
+
+.END
---
+++ tests/0_parsing/fill_variants.asm
@@ -0,0 +1,15 @@
+.ORIG x3000
+
+    halt
+
+    .FILL #0
+    .FILL #1
+    .FILL #-1
+    .FILL x0
+    .FILL x1
+    .FILL xF
+    .FILL -xF
+    .FILL 1
+    .FILL -1
+
+.END
